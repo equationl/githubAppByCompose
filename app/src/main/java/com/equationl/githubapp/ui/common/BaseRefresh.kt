5288e68e39cbee635245ba9ae86b2efeb3642451
equationl <equationl@qq.com>
2023-09-01 10:49:41 +0800

    增加下拉刷新动效

--- app/src/main/java/com/equationl/githubapp/ui/common/BaseRefresh.kt
+++ app/src/main/java/com/equationl/githubapp/ui/common/BaseRefresh.kt
@@ -1,16 +1,21 @@
 package com.equationl.githubapp.ui.common
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,13 +23,28 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.equationl.githubapp.R
 import com.equationl.githubapp.model.BaseUIModel
 import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.SwipeRefreshState
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 
+
+/**
+ * 下拉刷新动效与实际内容之间的间距
+ * */
+private const val SPACE = 20
 
 @Composable
 fun <T: BaseUIModel>BaseRefresh(
@@ -37,23 +57,62 @@ fun <T: BaseUIModel>BaseRefresh(
     onClickItem: (item: T) -> Unit,
     headerItem: (LazyListScope.() -> Unit)? = null,
 ) {
+    val refreshTriggerDistance = remember { 180.dp }
+
+    @Suppress("DEPRECATION")
     val rememberSwipeRefreshState = rememberSwipeRefreshState(isRefreshing = false)
+
+    var contentYOffsetDuration by remember { mutableStateOf(0) }
+    var contentYOffsetTarget by remember { mutableStateOf(0.dp) }
+    val contentYOffset by animateDpAsState(
+        targetValue = contentYOffsetTarget,
+        label = "contentYOffset",
+        animationSpec = tween(
+            durationMillis = contentYOffsetDuration
+        )
+    )
 
     rememberSwipeRefreshState.isRefreshing = isRefresh
 
+    @Suppress("DEPRECATION")
     SwipeRefresh(
         state = rememberSwipeRefreshState,
         onRefresh = onRefresh,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        indicator = { state, trigger ->
+            AnimationIndicator(
+                swipeRefreshState = state,
+                refreshTriggerDistance = trigger
+            )
+        },
+        refreshTriggerDistance = refreshTriggerDistance
     ) {
-        BaseRefreshLazyColumn(
-            itemList = itemList,
-            cacheItemList = cacheItemList,
-            lazyListState = lazyListState,
-            itemUi = itemUi,
-            onClickFileItem = onClickItem,
-            headerItem = headerItem,
-        )
+        if (rememberSwipeRefreshState.isRefreshing) {
+            contentYOffsetTarget = refreshTriggerDistance + SPACE.dp
+        }
+        else {
+            if (rememberSwipeRefreshState.isSwipeInProgress) {
+                with(LocalDensity.current) {
+                    contentYOffsetTarget = rememberSwipeRefreshState.indicatorOffset.toDp().coerceAtMost(refreshTriggerDistance + SPACE.dp)
+                }
+                contentYOffsetDuration = 0
+            }
+            else {
+                contentYOffsetTarget = 0.dp
+                contentYOffsetDuration = 300
+            }
+        }
+
+        Column(modifier = Modifier.offset(y = contentYOffset)) {
+            BaseRefreshLazyColumn(
+                itemList = itemList,
+                cacheItemList = cacheItemList,
+                lazyListState = lazyListState,
+                itemUi = itemUi,
+                onClickFileItem = onClickItem,
+                headerItem = headerItem,
+            )
+        }
     }
 }
 
@@ -101,9 +160,21 @@ fun <T : BaseUIModel>BaseRefreshPaging(
     onRefresh: (() -> Unit)? = null,
     emptyItem: @Composable () -> Unit = { EmptyItem() }
 ) {
+    val refreshTriggerDistance = remember { 180.dp }
     var isInitiativeRefresh by remember { mutableStateOf(false) }
 
+    @Suppress("DEPRECATION")
     val rememberSwipeRefreshState = rememberSwipeRefreshState(isRefreshing = false)
+
+    var contentYOffsetDuration by remember { mutableStateOf(0) }
+    var contentYOffsetTarget by remember { mutableStateOf(0.dp) }
+    val contentYOffset by animateDpAsState(
+        targetValue = contentYOffsetTarget,
+        label = "contentYOffset",
+        animationSpec = tween(
+            durationMillis = contentYOffsetDuration
+        )
+    )
 
     if (pagingItems?.loadState?.refresh is LoadState.Error) {
         onLoadError("加载失败！")
@@ -115,6 +186,7 @@ fun <T : BaseUIModel>BaseRefreshPaging(
         isInitiativeRefresh = false
     }
 
+    @Suppress("DEPRECATION")
     SwipeRefresh(
         state = rememberSwipeRefreshState,
         onRefresh = {
@@ -123,19 +195,44 @@ fun <T : BaseUIModel>BaseRefreshPaging(
             isInitiativeRefresh = true
         },
         modifier = Modifier.fillMaxSize(),
-        swipeEnabled = isScrollEnable
+        swipeEnabled = isScrollEnable,
+        indicator = { state, trigger ->
+            AnimationIndicator(
+                swipeRefreshState = state,
+                refreshTriggerDistance = trigger
+            )
+        },
+        refreshTriggerDistance = refreshTriggerDistance
     ) {
-        BasePagingLazyColumn(
-            pagingItems = pagingItems,
-            cacheItems = cacheItems,
-            itemUi = itemUi,
-            isRefresh = isInitiativeRefresh,
-            isScrollEnable = isScrollEnable,
-            lazyListState = lazyListState,
-            onClickItem = onClickItem,
-            emptyItem = emptyItem,
-            headerItem = headerItem
-        )
+        if (rememberSwipeRefreshState.isRefreshing) {
+            contentYOffsetTarget = refreshTriggerDistance + SPACE.dp
+        }
+        else {
+            if (rememberSwipeRefreshState.isSwipeInProgress) {
+                with(LocalDensity.current) {
+                    contentYOffsetTarget = rememberSwipeRefreshState.indicatorOffset.toDp().coerceAtMost(refreshTriggerDistance + SPACE.dp)
+                }
+                contentYOffsetDuration = 0
+            }
+            else {
+                contentYOffsetTarget = 0.dp
+                contentYOffsetDuration = 300
+            }
+        }
+
+        Column(modifier = Modifier.offset(y = contentYOffset)) {
+            BasePagingLazyColumn(
+                pagingItems = pagingItems,
+                cacheItems = cacheItems,
+                itemUi = itemUi,
+                isRefresh = isInitiativeRefresh,
+                isScrollEnable = isScrollEnable,
+                lazyListState = lazyListState,
+                onClickItem = onClickItem,
+                emptyItem = emptyItem,
+                headerItem = headerItem
+            )
+        }
     }
 }
 
@@ -177,7 +274,7 @@ private fun <T: BaseUIModel>BasePagingLazyColumn(
                             onClickItem(item)
                         }
                     ) {
-                        itemUi(data = item, isRefresh)
+                        itemUi(item, isRefresh)
                     }
                 }
             }
@@ -222,5 +319,73 @@ private fun <T: BaseUIModel>BasePagingLazyColumn(
                 }
             }
         }
+    }
+}
+
+@Suppress("DEPRECATION")
+@Composable
+private fun AnimationIndicator(
+    swipeRefreshState: SwipeRefreshState,
+    refreshTriggerDistance: Dp,
+) {
+    var tipText by remember { mutableStateOf("") }
+
+    val trigger = with(LocalDensity.current) { refreshTriggerDistance.toPx() }
+    val totalProgress = (swipeRefreshState.indicatorOffset / (trigger + with(LocalDensity.current) { SPACE.dp.toPx() * 3 })).coerceIn(0f, 1f)
+
+
+    var animationAlpha by remember { mutableStateOf(1f) }
+
+    var isPlaying by remember { mutableStateOf(false) }
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.loading))
+    val animationProgress by animateLottieCompositionAsState(composition, iterations = LottieConstants.IterateForever, isPlaying = isPlaying)
+
+    var animationTopOffset by remember { mutableStateOf(-refreshTriggerDistance) }
+
+    if (swipeRefreshState.isRefreshing) {
+        tipText = "正在加载中……"
+        animationTopOffset = 0.dp
+        isPlaying = true
+        animationAlpha = 1f
+    }
+    else {
+        animationTopOffset = with(LocalDensity.current) {
+            (swipeRefreshState.indicatorOffset.toDp() - refreshTriggerDistance).coerceAtMost(0.dp)
+        }
+
+        isPlaying = false
+        animationAlpha = totalProgress // FastOutSlowInEasing.transform(totalProgress)
+
+        tipText = if (swipeRefreshState.indicatorOffset < trigger) {
+            "继续下拉以刷新"
+        } else {
+            "松手立即刷新"
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(refreshTriggerDistance)
+            .offset(y = animationTopOffset),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        LottieAnimation(
+            composition = composition,
+            progress = {
+                if (swipeRefreshState.isRefreshing) {
+                    animationProgress
+                }
+                else {
+                    totalProgress
+                }
+            },
+            modifier = Modifier
+                .height(refreshTriggerDistance - SPACE.dp)
+                .alpha(animationAlpha)
+        )
+
+        Text(text = tipText)
     }
 }
