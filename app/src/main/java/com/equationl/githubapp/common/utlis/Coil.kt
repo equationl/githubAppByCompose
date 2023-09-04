d76ac6aacf23c630e3840ed536e4f85690e59ea0
equationl <equationl@qq.com>
2023-09-04 17:11:54 +0800

    图片预览支持 svg 和 gif

--- app/src/main/java/com/equationl/githubapp/common/utlis/Coil.kt
+++ app/src/main/java/com/equationl/githubapp/common/utlis/Coil.kt
@@ -1,12 +1,24 @@
 package com.equationl.githubapp.common.utlis
 
 import android.content.Context
+import android.os.Build.VERSION.SDK_INT
 import coil.ImageLoader
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.decode.SvgDecoder
 import coil.disk.DiskCache
 import coil.memory.MemoryCache
 
 fun Context.getImageLoader(): ImageLoader {
     return ImageLoader.Builder(this)
+        .components {
+            add(SvgDecoder.Factory())
+            if (SDK_INT >= 28) {
+                add(ImageDecoderDecoder.Factory())
+            } else {
+                add(GifDecoder.Factory())
+            }
+        }
         .respectCacheHeaders(false) // 禁用网络的缓存政策，不然不会拿本地缓存
         .memoryCache {
             MemoryCache.Builder(this)
